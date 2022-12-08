b81db48cfcbcaffccd632de5b8141cf205ca20e6
Ellie-Aghajani <z.aqajani@gmail.com>
2022-12-08 12:00:46 -0800

    use jQuery for form submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,70 +3,91 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
+
+$(function () {
+  //Escape method to avoid XSS attacks
+  const escape = function (str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
   }
-]
+  const createTweetElement = function(tweet){
+    let tweet = `
+        <article class="all-tweets-container">
+            <header class="tweet-header">
+                <div class="user">
+                    <img src="${tweet.user.avatars}" alt="profile-picture">
+                    <p>${tweet.user.handle}</p>
+                </div>
+                <p>${tweet.user.name}</p>
+            </header>
+            <h2> ${tweet.content.text}</h2>
+            <hr>
+            <footer>
+                <p> ${tweet.created_at}</p>
+                <div>
+                    <a href=""> <i class="fa-sharp fa-solid fa-flag"></i></a>
+                    <a href=""> <i class="fa-solid fa-arrows-rotate"></i></a>
+                    <a href=""><i class="fa-sharp fa-solid fa-heart"></i></a>
+            
+                </div>
+            </footer>
+        </article>`;
+    return tweet;   
+  };
 
-const createTweetElement = function(tweet){
-  let tweet = `
-      <article class="all-tweets-container">
-          <header class="tweet-header">
-              <div class="user">
-                  <img src="${tweet.user.avatars}" alt="profile-picture">
-                  <p>${tweet.user.handle}</p>
-              </div>
-              <p>${tweet.user.name}</p>
-          </header>
-          <h2> ${tweet.content.text}</h2>
-          <hr>
-          <footer>
-              <p> ${tweet.created_at}</p>
-              <div>
-                  <a href=""> <i class="fa-sharp fa-solid fa-flag"></i></a>
-                  <a href=""> <i class="fa-solid fa-arrows-rotate"></i></a>
-                  <a href=""><i class="fa-sharp fa-solid fa-heart"></i></a>
-          
-              </div>
-          </footer>
-      </article>`;
-
-
-
-  return tweet;   
-};
-
-const $tweet = createTweetElement(tweet);
-$('#tweets').append($tweet);
-
-const renderTweets = function (tweets) {
-  $('#tweets').empty();
-
-  for (let tweet of tweets) {
-    const newTweet = createTweetElement(tweet);
-
-    $('#tweets').prepend(newTweet);
+  const loadTweets = function () {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET'
+    })
+      .done((tweets) => {
+        renderTweets(tweets)
+      })
+      .fail(() => console.log('An error has occurred'))
+      .always(() => console.log('Succesful request'));
   }
-}
-renderTweets(data);
+
+  const renderTweets = function (tweets) {
+    $('#tweets').empty();
+
+    for (let tweet of tweets) {
+      const newTweet = createTweetElement(tweet);
+
+      $('#tweets').prepend(newTweet);
+    }
+  }
+   //tweet creation method
+   $('#new-tweet-form').on('submit', function (event) {
+    event.preventDefault();
+
+    const $tweetText = $(this).children('#tweet-text');
+
+    if ($tweetText.val().length > 140) {
+      $('.error').html(`<p>There is a limit for written characters (140), Please reduce the text </p>`);
+      $('.error').slideDown('slow');
+      return;
+    } else if ($tweetText.val().length === 0) {
+      $('.error').html(`<p>Please write something to tweet :D</p>`);
+      $('.error').slideDown('slow');
+      return
+    }
+
+    const formContent = $(this).serialize();
+
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: formContent
+    })
+      .done(() => loadTweets())
+      .fail(() => console.log("Something went wrong"))
+      .always(() => console.log("SUCCESS BAYBEE"));
+
+    $("#tweet-text").val("");
+    $(this).find('.counter').val('140');
+
+  });
+
+  loadTweets();
+})
